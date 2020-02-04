14940397c776620fb207f88db64476eb53468e13
Ali Jabakhanji <ali.jabakhanji@hotmail.com>
2020-02-04 11:04:46 -0500

    deuxieme commit. Creation de l'objet CV

--- src/tp1/CV.java
+++ src/tp1/CV.java
@@ -2,4 +2,22 @@ package tp1;
 
 public class CV {
 
+	static private String nom;
+	static private String prenom;
+	static private String formation;
+	static private int experience;
+	static private String[] competences;
+	static private String attente;
+	
+	public CV (String pNom, String pPrenom, String pFormation, int pExperience, String[] pCompetences, String pAttente) {
+		nom = pNom;
+		prenom = pPrenom;
+		formation = pFormation;
+		experience = pExperience;
+		competences = pCompetences;
+		attente = pAttente;
+	}
+	
+	
+	
 }
