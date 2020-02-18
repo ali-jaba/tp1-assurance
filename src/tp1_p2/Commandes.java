ef0b402ba388884ce57d917e60e550f109b333a0
Ali Jabakhanji <ali.jabakhanji@hotmail.com>
2020-02-18 13:18:23 -0500

    afficher prix

--- src/tp1_p2/Commandes.java
+++ src/tp1_p2/Commandes.java
@@ -9,11 +9,39 @@ public class Commandes {
 	public int qnt;
 	
 	
+	
 	public Commandes(String nom, String plat, int qnt) {
 		super();
 		this.nom = nom;
 		this.plat = plat;
 		this.qnt = qnt;
+	}
+	
+	public double prixTotal(int qnt, String plat) {
+		double prix = 0;
+		
+		double prixFinal;
+
+		switch (plat) {
+
+		case "Poutine":
+			prix = 10.5;
+			break;
+		case "Frites":
+			prix = 2.5;
+			break;
+		case "Repas_Poulet":
+			prix = 15.75;
+			break;
+		}
+
+		prixFinal = prix * qnt;
+
+		return prixFinal ;
+	}
+	public void afficher() {
+		System.out.println(this.nom);
+		System.out.println(prixTotal(qnt, plat));
 	}
 	
 	
--- src/tp1_p2/principal.java
+++ src/tp1_p2/principal.java
@@ -8,16 +8,11 @@ import java.io.IOException;
 
 public class principal {
 
-	
-	
 	public static void main(String[] args) throws IOException {
 		
 		String[] tableau = lireFichier(new File("entree.txt").getAbsolutePath());
 		
-		Plats[] tabPlats = new Plats[3];
-		tabPlats[0] = new Plats(tableau[5], Double.valueOf(tableau[6]));
-		tabPlats[1] = new Plats(tableau[7], Double.valueOf(tableau[8]));
-		tabPlats[2] = new Plats(tableau[9], Double.valueOf(tableau[10]));
+		
 		
 
 		
@@ -26,55 +21,34 @@ public class principal {
 		tabCli[1] = new Client(tableau[2]);
 		tabCli[2] = new Client(tableau[3]);
 		
-		Commandes[] tabComm = new Commandes[3];
 		
+		Commandes[] tabComm = new Commandes[3];
 		tabComm[0] = new Commandes(tableau[12], tableau[13], Integer.parseInt(tableau[14]));
 		tabComm[1] = new Commandes(tableau[15], tableau[16], Integer.parseInt(tableau[17]));
 		tabComm[2] = new Commandes(tableau[18], tableau[19], Integer.parseInt(tableau[20]));
-	}
-	
-	public static void afficherFac(String client,  ) {
 		
-	}
-	
-	public static double prixCom(Plats plats, int quantite) {
-		double prix =0;
-		double prixFinal;
+		tabComm[0].afficher();
+		tabComm[1].afficher();
+		tabComm[2].afficher();
 		
-		String nomPlats = plats.getNom();
-		
-		switch (nomPlats) {
-		
-		case "Poutine" :
-			prix = 10.5;
-			break;
-		case "Frites":
-			prix = 2.5; 
-			break;
-		case "Repas_Poulet":
-			prix = 15.75;
-			break;
-		}
-		
-		prixFinal = prix * quantite;
-		
-		
-		return prix;
 		
 	}
+
 	
+
 	
-	public static String[] lireFichier( String chemin ) throws FileNotFoundException, IOException {
+
+	public static String[] lireFichier(String chemin) throws FileNotFoundException, IOException {
 
 		String[] tab;
 
-		try (BufferedReader br = new BufferedReader( new FileReader( chemin ) )) {
+		try (BufferedReader br = new BufferedReader(new FileReader(chemin))) {
 			String ligne;
 
-			 ligne = br.readLine();
-				tab = ligne.split(";");
-			}
-		
+			ligne = br.readLine();
+			tab = ligne.split(";");
+		}
+
 		return tab;
 	}
 }
