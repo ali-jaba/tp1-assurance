c8f11d8895d6a8f04188d72cabf24457379705d4
Ali Jabakhanji <ali.jabakhanji@hotmail.com>
2020-04-21 14:46:02 -0400

    Version final

--- src/tp1_p3_test/test.java
+++ src/tp1_p3_test/test.java
@@ -1,6 +1,8 @@
 package tp1_p3_test;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -19,6 +21,9 @@ public class test {
 		double prix = test.prixTotal(3, "Poutine");
 		double prix2 = test2.prixTotal(0, "fromage");
 		
+		assertEquals(36.14625, prix);
+		assertEquals(0.0, prix2);
+		
 
 		
 		principal.lireFichier(new File("entree.txt").getAbsolutePath());
@@ -30,10 +35,9 @@ public class test {
 	public void creationFacture() throws IOException {
 		String tab[] = {"Clients","Roger","Céline","Steeve","Plats","Poutine","10.5","Frites","2.5","Repas_Poulet","15.75","Commandes","Roger","Poutine","1","Céline","Frites","2","Céline","Repas_Poulet","1","Fin"};
 		principal.main(tab);
+		principal.lireFichier(new File("entree.txt").getAbsolutePath());
 	}
-	@Test
-	public void commande() throws IOException {
-		
-	}
+	
+	
 	
 }
